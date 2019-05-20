49a508e1dc91aba4663e25bd9bfe479f7ea11785
D12thelf <kri17005@byui.edu>
2019-05-20 09:44:44 -0700

    234

--- js/lab4-2.js
+++ js/lab4-2.js
@@ -4,7 +4,7 @@ INPUT:  Obtain three inputs with City, State and Zip Code
 PROCESSING:  Create address by joining City, State and Zip Code to equal the address.
 OUTPUT:  Create and display address
 */
-//INPUT
+
 function addressCreator() {
 	//INPUT
 	let city = document.getElementById('city').value;
